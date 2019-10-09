6f13a535a2327238ccd97513474a3cbc6728db4f
Hipolito Castillo Juan Francisco <juanfranciscoutn@gmail.com>
2019-10-08 19:20:39 -0500

    Cambios minimos

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const morgan_1 = __importDefault(require("morgan"));
+const body_parser_1 = __importDefault(require("body-parser"));
+// Importa ruas
 const auth_1 = __importDefault(require("./routes/auth"));
 const mascota_1 = __importDefault(require("./routes/mascota"));
-const morgan_1 = __importDefault(require("morgan"));
+// Inicializar Variables
 const app = express_1.default();
+// Body parser
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
 //settings
 app.set('port', process.env.PORT || 3000);
 // middlewares
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express, { Application } from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+// Importa ruas
 import authRoutes from './routes/auth';
 import mascotaRoutes from './routes/mascota';
-import morgan from 'morgan';
 
+// Inicializar Variables
 const app: Application = express();
+
+// Body parser
+app.use( bodyParser.urlencoded({ extended: false }));
+app.use( bodyParser.json() );
 
 //settings
 app.set('port', process.env.PORT || 3000);
